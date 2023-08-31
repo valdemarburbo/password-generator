b34c32887444a2b72c975b3239c50800eeb0811a
Valdemar Burbo <valdemar.burbo@gmail.com>
2023-08-31 12:08:46 +0100

    Finish random password generator logic

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
-passOne = document.getElementById("pass-one");
-passTwo = document.getElementById("pass-two");
+let passOne = document.getElementById("pass-one");
+let passTwo = document.getElementById("pass-two");
 
 function getRandomIndex() {
    return Math.floor(Math.random() * characters.length)
 }
 
 function generatePasswords() {
-    let randomIndexOne = getRandomIndex();
-    let randomIndexTwo = getRandomIndex();
+    passOne.textContent = "";
+    passTwo.textContent = "";
 
     for (let i = 0; i < 15; i++ ) {
+        let randomIndexOne = getRandomIndex();
+        let randomIndexTwo = getRandomIndex();
 
+        passOne.textContent += characters[randomIndexOne]
+        passTwo.textContent += characters[randomIndexTwo]
     }
 }
 
