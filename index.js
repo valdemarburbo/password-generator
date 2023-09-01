2f6d664e6d22a09142e656b6eb2426f275c9cbb0
Valdemar Burbo <valdemar.burbo@gmail.com>
2023-09-01 13:35:25 +0100

    Comment out copy function

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ function setSpecial() {
     characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 }
 
-function copyPassword() {
-    passOne.select();
-    // passOne.setSelectionRange(0, 99999);
+// function copyPassword() {
+//     passOne.select();
+//     // passOne.setSelectionRange(0, 99999);
 
-    navigator.clipboard.writeText(passOne.value)
-}
+//     navigator.clipboard.writeText(passOne.value)
+// }
