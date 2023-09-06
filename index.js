186ed95060f2d8eb6d6779bd7dc6c704f2f83d3d
Valdemar Burbo <valdemar.burbo@gmail.com>
2023-09-07 00:50:25 +0100

    Refactor function to change characters array

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 let characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
+const lengthEight = document.getElementById("length-8")
+const lengthSixteen = document.getElementById("length-16")
+const lengthTwentyFour = document.getElementById("length-24")
+
+const everythingOption = document.getElementById("everything")
+const numbersOption = document.getElementById("numbers")
+const specialOption = document.getElementById("special")
+
 let passOne = document.getElementById("pass-one");
 let passTwo = document.getElementById("pass-two");
 let passLength = 16;
@@ -21,28 +29,44 @@ function generatePasswords() {
     }
 }
 
-function setLength8() {
-    passLength = 8;
+function setLength(value) {
+    passLength = value;
+    if (passLength === 8) {
+        lengthEight.classList.add("clicked")
+        lengthSixteen.classList.remove("clicked")
+        lengthTwentyFour.classList.remove("clicked")
+    } else if (passLength === 16) {
+        lengthSixteen.classList.add("clicked")
+        lengthEight.classList.remove("clicked")
+        lengthTwentyFour.classList.remove("clicked")
+    } else if (passLength === 24) {
+        lengthTwentyFour.classList.add("clicked")
+        lengthSixteen.classList.remove("clicked")
+        lengthEight.classList.remove("clicked")
+    }
 }
 
-function setLength16() {
-    passLength = 16;
-}
+function setCharacterOption(value) {
+    let optionSelection = value
+    if (optionSelection === everything) {
+        characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
-function setLength24() {
-    passLength = 24;
-}
+        everythingOption.classList.add("clicked")
+        numbersOption.classList.remove("clicked")
+        specialOption.classList.remove("clicked")
+    } else if (optionSelection === numbers) {
+        characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-function setEverything() {
-    characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
-}
+        numbersOption.classList.add("clicked")
+        everythingOption.classList.remove("clicked")
+        specialOption.classList.remove("clicked")
+    } else if (optionSelection === special) {
+        characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
-function setNumbers() {
-    characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-}
-
-function setSpecial() {
-    characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+        specialOption.classList.add("clicked")
+        numbersOption.classList.remove("clicked")
+        everythingOption.classList.remove("clicked")
+    }
 }
 
 function copyPassword(string) {
